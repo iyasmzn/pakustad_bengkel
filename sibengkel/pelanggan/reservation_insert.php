8d72507e57889a3678aadd2cdc16e04d2e94dad4
iyasmzn <iyasmzn07@gmail.com>
2025-03-05 22:17:01 +0700

    `Added input sanitization and new field 'nopol' to reservation_insert.php`

--- sibengkel/pelanggan/reservation_insert.php
+++ sibengkel/pelanggan/reservation_insert.php
@@ -2,13 +2,13 @@
 include("sess_check.php");
 
 $id = $sess_pelangganid;
-$tanggal = $_POST['tanggal'];
-$keluhan = $_POST['keluhan'];
+$tanggal = mysqli_real_escape_string($conn, $_POST['tanggal']);
+$keluhan = mysqli_real_escape_string($conn, $_POST['keluhan']);
+$nopol = mysqli_real_escape_string($conn, $_POST['nopol']);
 $status = 0;
-// $keterangan = $_POST['keterangan'];
 
-$sql = "INSERT INTO reservations(tanggal,keluhan,status,id_pelanggan)
-		  VALUES('$tanggal','$keluhan','$status','$id')";
+$sql = "INSERT INTO reservations(tanggal,keluhan,status,id_pelanggan,nopol)
+		  VALUES('$tanggal','$keluhan','$status','$id','$nopol')";
 $ress = mysqli_query($conn, $sql);
 if ($ress) {
 	echo "<script>alert('Reservasi Berhasil!');</script>";
