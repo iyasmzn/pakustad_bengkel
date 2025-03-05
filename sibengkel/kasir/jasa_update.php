040eab6842234ddf8fc261203a15b20ddb2a0d88
iyasmzn <iyasmzn07@gmail.com>
2025-03-05 15:08:32 +0700

    update kasir role

--- sibengkel/kasir/jasa_update.php
+++ sibengkel/kasir/jasa_update.php
@@ -1,19 +1,23 @@
 <?php
-	include("sess_check.php");
-	
-	// query database memperbarui data pada database
-	if(isset($_POST['perbarui'])) {
-		$nama=$_POST['nama'];
-		$harga=$_POST['harga'];
-		$keterangan=$_POST['keterangan'];
-		$brg=$_POST['id'];
-		
-		$sql = "UPDATE barangjasa SET
-				nama='". $nama ."',
-				harga='". $harga ."',
-				keterangan='". $keterangan ."'
-				WHERE id_brg='". $brg ."'";
-		$ress = mysqli_query($conn, $sql);
-		header("location: jasa.php?act=update&msg=success");
+include("sess_check.php");
+
+// query database memperbarui data pada database
+if (isset($_POST['perbarui'])) {
+	$jenis = $_POST['jenis'];
+	$harga = $_POST['harga'];
+	$brg = $_POST['id'];
+
+	$sql = "UPDATE jasa_servis SET
+				jenis='" . $jenis . "',
+				harga='" . $harga . "'
+				WHERE id='" . $brg . "'";
+	$ress = mysqli_query($conn, $sql);
+	if ($ress) {
+		echo "<script>alert('Ubah Data Berhasil!');</script>";
+		echo "<script type='text/javascript'> document.location = 'jasa.php'; </script>";
+	} else {
+		echo ("Error description: " . mysqli_error($conn));
+		echo "<script>alert('Ops, terjadi kesalahan. Silahkan coba lagi.');</script>";
+		echo "<script type='text/javascript'> document.location = 'jasa.php'; </script>";
+	}
 }
-?>
