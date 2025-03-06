98bc34e53fcbba0bfa4f361b5339033b74936bf3
iyasmzn <iyasmzn07@gmail.com>
2025-03-06 11:42:14 +0700

    `Added filter for karyawan to index.php and corresponding JavaScript code`

--- sibengkel/index.php
+++ sibengkel/index.php
@@ -4,10 +4,11 @@ include("dist/function/format_rupiah.php");
 
 $tgl = date('Y-m-d');
 $ttl = 0;
+$filter_karyawan = $_GET['filter_k'] ?? null;
 $sql = "SELECT reservations.*, sparepart.harga as harga_sparepart, jasa_servis.harga as harga_jasa FROM reservations 
     LEFT JOIN sparepart ON sparepart.kode=reservations.kode_sparepart
     LEFT JOIN jasa_servis ON jasa_servis.id=reservations.id_jasa_servis
-WHERE tanggal='$tgl'";
+WHERE tanggal='$tgl'" . ($filter_karyawan ? "AND id_karyawan = '$filter_karyawan'" : "");
 $ress = mysqli_query($conn, $sql);
 $jmltrx = mysqli_num_rows($ress);
 // query database mencari data admin
@@ -31,6 +32,23 @@ include("layout_top.php");
             </div><!-- /.col-lg-12 -->
         </div><!-- /.row -->
 
+        <!-- Filter Karyawan -->
+        <div class="row">
+            <div class="col-lg-6">
+                <select id="input-filter_k" name="id_karyawan" class="form-control" required style="margin-bottom: 10px;">
+                    <option value="">Pilih Karyawan</option>
+                    <?php
+                    $sqlKaryawan = "SELECT * FROM karyawan ORDER BY nama_karyawan ASC";
+                    $ressKaryawan = mysqli_query($conn, $sqlKaryawan);
+                    while ($dataKaryawan = mysqli_fetch_array($ressKaryawan)) {
+                        $selectedKaryawan = ($dataKaryawan['id_karyawan'] == $filter_karyawan) ? 'selected' : '';
+                        echo "<option value='" . $dataKaryawan['id_karyawan'] . "' $selectedKaryawan>" . $dataKaryawan['nama_karyawan'] . "</option>";
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-lg-6 col-md-6">
                 <div class="panel panel-primary">
@@ -42,7 +60,7 @@ include("layout_top.php");
                             <div class="col-xs-9 text-right">
                                 <div class="huge"><?php echo $jmltrx; ?></div>
                                 <div>
-                                    <h4>Reservasi Hari Ini</h4>
+                                    <h4>Transaksi Hari Ini</h4>
                                 </div>
                             </div>
                         </div>
@@ -85,6 +103,19 @@ include("layout_top.php");
     </div><!-- /.container-fluid -->
 </div><!-- /#page-wrapper -->
 <!-- bottom of file -->
+<script>
+    $('document').ready(function() {
+        $('#input-filter_k').on('change', function() {
+            if (isNaN(this.value))
+                return;
+            else {
+                let url = new URL(window.location.href);
+                url.searchParams.set('filter_k', this.value);
+                window.location.href = url.href;
+            }
+        });
+    })
+</script>
 <?php
 include("layout_bottom.php");
 ?>
