3076233b8697dbac16370c8462ea9e6eb974007f
iyasmzn <iyasmzn07@gmail.com>
2025-03-06 09:28:03 +0700

    `Updated laporan_trx_cetak.php to use new database schema and table names`

--- sibengkel/laporan_trx_cetak.php
+++ sibengkel/laporan_trx_cetak.php
@@ -1,20 +1,27 @@
 <?php
-	include("sess_check.php");
+include("sess_check.php");
 
-	include("dist/function/format_tanggal.php");
-	include("dist/function/format_rupiah.php");
-	$mulai 	 = $_GET['awal'];
-	$selesai = $_GET['akhir'];
-	$sql = "SELECT trx.*, konsumen.*, kasir.* FROM trx, konsumen, kasir WHERE
-			trx.id_kon=konsumen.id_kon AND trx.id_kasir=kasir.id_kasir AND 
-			trx.tgl_trx BETWEEN '$mulai' AND '$selesai' ORDER BY trx.id_trx DESC";
-	$ress = mysqli_query($conn, $sql);
-	// deskripsi halaman
-	$pagedesc = "Laporan Data Transaksi - Periode " . IndonesiaTgl($mulai) ." - ". IndonesiaTgl($selesai);
-	$pagetitle = str_replace(" ", "_", $pagedesc)
+include("dist/function/format_tanggal.php");
+include("dist/function/format_rupiah.php");
+$mulai 	 = $_GET['awal'];
+$selesai = $_GET['akhir'];
+$sql = "SELECT reservations.*, pelanggan.*, karyawan.*, 
+            sparepart.harga as harga_sparepart,
+            jasa_servis.harga as harga_jasa
+            FROM reservations
+            LEFT JOIN pelanggan ON pelanggan.id_pelanggan=reservations.id_pelanggan
+            LEFT JOIN karyawan ON karyawan.id_karyawan=reservations.id_karyawan
+            LEFT JOIN sparepart ON sparepart.kode=reservations.kode_sparepart
+            LEFT JOIN jasa_servis ON jasa_servis.id=reservations.id_jasa_servis
+            WHERE reservations.tanggal BETWEEN '$mulai' AND '$selesai' ORDER BY reservations.id_reservation DESC";
+$ress = mysqli_query($conn, $sql);
+// deskripsi halaman
+$pagedesc = "Laporan Data Transaksi - Periode " . IndonesiaTgl($mulai) . " - " . IndonesiaTgl($selesai);
+$pagetitle = str_replace(" ", "_", $pagedesc)
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -35,7 +42,7 @@
 
 	<!-- Custom Fonts -->
 	<link href="libs/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	
+
 	<!-- jQuery -->
 	<script src="libs/jquery/dist/jquery.min.js"></script>
 
@@ -57,9 +64,9 @@
 							<img src="foto/logo.png" alt="logo-dkm" width="70" />
 						</td>
 						<td class="text-center" width="60%">
-						<b>Bengkel Mantap Jiwa</b> <br>
-						Bekasi<br>
-						Telp: (021) 192819189<br>
+							<b>SiBengkel</b> <br>
+							Semarang<br>
+							Telp: (021) 192819189<br>
 						<td class="text-right" width="20%">
 						</td>
 					</tr>
@@ -71,45 +78,47 @@
 
 	<section id="body-of-report">
 		<div class="container-fluid">
-			<h5 class="text-center">Laporan Transaksi Periode Tanggal <?php echo format_tanggal($mulai);?> s/d <?php echo format_tanggal($selesai);?></h5>
+			<h5 class="text-center">Laporan Transaksi Periode Tanggal <?php echo format_tanggal($mulai); ?> s/d <?php echo format_tanggal($selesai); ?></h5>
 			<br />
-								<table class="table table-striped table-bordered table-hover" id="tabel-data">
-									<thead>
-										<tr>
-											<th width="1%">No</th>
-											<th width="10%">ID Trx</th>
-											<th width="10%">Tgl Trx</th>
-											<th width="10%">Konsumen</th>
-											<th width="10%">Kasir</th>
-											<th width="10%">Total</th>
-										</tr>
-									</thead>
-									<tbody>
-										<?php
-											$ttl=0;
-											$i = 1;
-											while($data = mysqli_fetch_array($ress)) {
-												$tot = $data['total'];
-												echo '<tr>';
-												echo '<td class="text-center">'. $i .'</td>';
-												echo '<td class="text-center">'. $data['id_trx'] .'</td>';
-												echo '<td class="text-center">'. format_tanggal($data['tgl_trx']) .'</td>';
-												echo '<td class="text-center">'. $data['nama_kon'] .'</td>';
-												echo '<td class="text-center">'. $data['nama_kasir'] .'</td>';
-												echo '<td class="text-center">'. format_rupiah($data['total']) .'</td>';
-												echo '</tr>';												
-												$i++;
-												$ttl+=$tot;
-											}
-										?>
-									<tfoot>
-										<tr>
-											<th colspan="5" class="text-center">Total </th>
-											<th class="text-right"><?php echo format_rupiah($ttl);?></th>
-										</tr>
-									</tfoot>
-									</tbody>
-								</table>
+			<table class="table table-striped table-bordered table-hover" id="tabel-data">
+				<thead>
+					<tr>
+						<th width="1%">No</th>
+						<th width="10%">ID Trx</th>
+						<th width="10%">Tgl Trx</th>
+						<th width="10%">Konsumen</th>
+						<th width="10%">Kasir</th>
+						<th width="10%">Total</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$ttl = 0;
+					$i = 1;
+					while ($data = mysqli_fetch_array($ress)) {
+						$hargaSparepart = $data['harga_sparepart'] ?? 0;
+						$hargaJasa = $data['harga_jasa'] ?? 0;
+						$tot = $hargaSparepart + $hargaJasa;
+						echo '<tr>';
+						echo '<td class="text-center">' . $i . '</td>';
+						echo '<td class="text-center">' . $data['id_reservation'] . '</td>';
+						echo '<td class="text-center">' . $data['tanggal'] . '</td>';
+						echo '<td class="text-center">' . $data['nama'] . '</td>';
+						echo '<td class="text-center">' . $data['nama_karyawan'] . '</td>';
+						echo '<td class="text-center">' . format_rupiah($tot) . '</td>';
+						echo '</tr>';
+						$i++;
+						$ttl += $tot;
+					}
+					?>
+				<tfoot>
+					<tr>
+						<th colspan="5" class="text-center">Total </th>
+						<th class="text-right"><?php echo format_rupiah($ttl); ?></th>
+					</tr>
+				</tfoot>
+				</tbody>
+			</table>
 			<br />
 		</div><!-- /.container -->
 	</section>
@@ -126,4 +135,5 @@
 	<script src="libs/jTerbilang/jTerbilang.js"></script>
 
 </body>
+
 </html>
