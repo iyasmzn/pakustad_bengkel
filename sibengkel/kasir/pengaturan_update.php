c332dddc2cf63588d160041849ad9df8ebf769aa
iyasmzn <iyasmzn07@gmail.com>
2025-03-06 16:15:10 +0700

    `Fixed typo in table and column names in pengaturan.php and pengaturan_update.php`

--- sibengkel/kasir/pengaturan_update.php
+++ sibengkel/kasir/pengaturan_update.php
@@ -1,27 +1,24 @@
 <?php
-	include("sess_check.php");
-	
-	// query database memperbarui data pada database
-	if(isset($_POST['perbarui'])) {
-		$id_pengguna = $_POST['id_pengguna'];
-		$password_old = $_POST['password_old'];
-		$password_old2 = $_POST['password_old2'];
-		$password_new = $_POST['password_new'];
-		$password_new2 = $_POST['password_new2'];
-		
-		if($password_old == $password_old2) {
-			if($password_new == $password_new2) {
-				$sql = "UPDATE admin SET pass_kasir='". $password_new ."' WHERE id_kasir='". $id_pengguna ."'";
-				$ress = mysqli_query($conn, $sql);
-				
-				header("location: pengaturan.php?act=update&msg=success");
-			}
-			else {
-				header("location: pengaturan.php?act=update&msg=pwd_err_2");
-			}
+include("sess_check.php");
+
+// query database memperbarui data pada database
+if (isset($_POST['perbarui'])) {
+	$id_pengguna = $_POST['id_pengguna'];
+	$password_old = $_POST['password_old'];
+	$password_old2 = $_POST['password_old2'];
+	$password_new = $_POST['password_new'];
+	$password_new2 = $_POST['password_new2'];
+
+	if ($password_old == $password_old2) {
+		if ($password_new == $password_new2) {
+			$sql = "UPDATE karyawan SET pass_karyawan='" . $password_new . "' WHERE id_karyawan='" . $id_pengguna . "'";
+			$ress = mysqli_query($conn, $sql);
+
+			header("location: pengaturan.php?act=update&msg=success");
+		} else {
+			header("location: pengaturan.php?act=update&msg=pwd_err_2");
 		}
-		else {
-			header("location: pengaturan.php?act=update&msg=pwd_err_1");
-		}
+	} else {
+		header("location: pengaturan.php?act=update&msg=pwd_err_1");
 	}
-?>
+}
